db52ae624f9b0a01cfef5bbe44cbacc716c66078
miltonhuynh <miltonhuynh@gmail.com>
2022-05-09 19:47:43 -0700

    Added go to desktop version

--- app/src/Components/HeaderFooter.js
+++ app/src/Components/HeaderFooter.js
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faDesktop } from '@fortawesome/free-solid-svg-icons'
 import { ReactComponent as Githubicon } from '../github-brands.svg';
 
 export default function HeaderFooter(props) {
@@ -23,7 +24,12 @@ export default function HeaderFooter(props) {
     <>
       <div id="Header">
         <div id="Brand" onClick={goHome}>The React Store</div>
-        <div id="Github-icon"><a href="https://github.com/miltonhuynh/React-Store"><Githubicon/></a></div>
+        <div>
+          <a href="https://miltonhuynh.github.io/React-Store/">
+            <FontAwesomeIcon icon={faDesktop} id="Mobile-icon"/>
+          </a>
+        </div>
+        <div id="Github-icon"><a href="https://github.com/miltonhuynh/Mobile-React-Store"><Githubicon/></a></div>
         <div id="Navbar">
           <div className="Nav-icon" onClick={goHome}><FontAwesomeIcon icon={faHome} /></div>
           <div className="Nav-icon" onClick={goCart}><FontAwesomeIcon icon={faCartShopping} /></div>
